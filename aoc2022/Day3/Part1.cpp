374dd524c80e7a86d689a02a1f45a0953cb95fb7
TheApplePieGod <evant.002@gmail.com>
2022-12-03 11:24:12 -0500

    Day3

--- aoc2022/Day3/Part1.cpp
+++ aoc2022/Day3/Part1.cpp
@@ -5,9 +5,32 @@
 
 int main(int argc, char** argv)
 {
-    auto data = FilesystemUtils::ReadAsString("../../Day3/input.txt");
+    auto lines = FilesystemUtils::ReadLines("../../Day3/input.txt");
     
-    //std::cout << "Max: " << max << std::endl;
+    int score = 0;
+    for (auto& line : lines)
+    {
+        int start0 = 0;
+        int start1 = line.size() / 2;
+
+        while (line[start0] != line[start1])
+        {
+            start1++;
+            if (start1 >= line.size())
+            {
+                start1 = line.size() / 2;
+                start0++;
+            }
+        }
+        
+        char same = line[start0];
+        if (same >= 'a' && same <= 'z')
+            score += same - 'a' + 1;
+        else
+            score += same - 'A' + 27;
+    }
+
+    std::cout << "Value: " << score << std::endl;
 
     return 0;
 }
--- aoc2022/Day3/Part2.cpp
+++ aoc2022/Day3/Part2.cpp
@@ -3,31 +3,37 @@
 #include "Base.h"
 #include "Utils.h"
 
+bool find(const std::string& str, char val)
+{
+    for (auto c : str)
+        if (c == val) return true;
+    return false;
+}
+
 int main(int argc, char** argv)
 {
-    auto data = FilesystemUtils::ReadAsString("../../Day1/input.txt");
-    auto elves = StringUtils::Split(data, "\n\n");
+    auto lines = FilesystemUtils::ReadLines("../../Day3/input.txt");
     
-    std::vector<int> totals;
-    for (auto& data : elves)
+    int score = 0;
+    for (int i = 0; i < lines.size(); i += 3)
     {
-        auto values = StringUtils::Split(data, "\n");
-        int sum = 0;
-        for (auto& value : values)
+        char same;
+        for (auto c : lines[i])
         {
-            int num = stoi(value);
-            sum += num;
+            if (find(lines[i + 1], c) && find(lines[i + 2], c))
+            {
+                same = c;
+                break;
+            }
         }
-        totals.push_back(sum);
+        
+        if (same >= 'a' && same <= 'z')
+            score += same - 'a' + 1;
+        else
+            score += same - 'A' + 27;
     }
-    
-    std::sort(totals.begin(), totals.end());
-    
-    int top = totals.back();
-    top += totals[totals.size() - 2];
-    top += totals[totals.size() - 3];
-    
-    std::cout << "Top: " << top << std::endl;
+
+    std::cout << "Value: " << score << std::endl;
 
     return 0;
 }
